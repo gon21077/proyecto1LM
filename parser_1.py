62fab05221307b709c7c84bac3a6514be9cfd67e
gua21150 <gua21150@uvg.edu.gt>
2023-08-08 19:48:35 -0600

    Algunas correcciones

--- parser_1.py
+++ parser_1.py
@@ -5,6 +5,11 @@ import pandas as pd
 
 data = read_csv('BL-Flickr-Images-Book.csv')
 
+def coma_seguido_coma(texto):
+  temp = re.sub(r'((?<=\,)(?=,))',"  ", texto) # agrega dos espacios en los casos de comas seguidas
+  temp = re.sub(r'^0{6}|^0{5}|^0{4}|^0{3}|^0{2}|^0{1}',"", temp) # elimina los 0 previamente 
+  return temp
+
 def get_encabezados(d):
     linea_encabezados = d[0]
     temp = []
@@ -22,7 +27,7 @@ def parse_linea(l):
     #temp = re.findall(r'["]+.*(.*["]["].*["]["].*)+.*["][,]|["][^"]*["][,]|["][^"]*["]\n|[^,]*[,]|.*\n', l)    
     #temp = re.findall(r'["][^"]*["][,]|["][^"]*["]\n|[^,]*[,]|.*\n', l)
     temp = re.findall(r'["].*["][,]|["][^"]*["]\n|[^,]*[,]|.*\n', l)
-    #temp = re.findall(r'["](.*"".*"".*)+["]([,]|\n)|["][^"]*["]([,]|\n)|[^,]*([,]|\n)', l)
+    #temp = re.findall(r'["](.*"".*"".*)+["][,]|\n)|["][^"]*["]([,]|\n)|[^,]*([,]|\n)', l)
     temp[-1] = temp[-1].strip("\n")
     #print(temp)
     #temp = [" ".join(x) for x in temp ]
@@ -32,35 +37,50 @@ def parse_linea(l):
     
     return temp
 #["](.*"".*"".*)+.*["][,]
+
+def parse_linea_mariel(l):  
+    dat = re.findall(r'(?<=[\"\[]).[^\"]*[\"\]]\,?|[^\,\"]*\,?', l) # realiza la separacion 
+    lista_filtrada = [elemento for elemento in dat if elemento != ""] # incluye los elementos que son diferentes de vacio 
+    for i in range(len(lista_filtrada)-1):
+        lista_filtrada[i] = re.sub(r'\,$|(\"\,)$',"", lista_filtrada[i]) # elimina comas y o comas comilla doble
+      
+    lista_filtrada[-1] = lista_filtrada[-1].strip("\n")
+    return lista_filtrada
+
+
 lineas = []
 for i in range(len(data)):
     if i == 0:
-        continue
-    lineas.append(parse_linea(data[i]))
-    if len(parse_linea(data[i]))>15:
-        print("Errror en linea ", i)
+        continue    
+    x = coma_seguido_coma(data[i])
+    temp = parse_linea(x)
+    
+    lineas.append(temp)
+    if len(temp)>15:
+        print("Error en linea ", i)
+        print(temp)
    
 
 
 
-df = pd.DataFrame(lineas,columns=encabezados)
+df = pd.DataFrame(lineas, columns=encabezados)
 
 dfinicial = pd.read_csv('BL-Flickr-Images-Book.csv')
 
-#print(df.columns)
-#print(dfinicial.columns)
+print(df.columns)
+print(dfinicial.columns)
 
 
-#print(dfinicial.head())
-#print(df.head())
+print(dfinicial.head())
+print(df.head())
 diferentes = df != dfinicial
 
 valores_diferentes = df[diferentes].fillna('Diferente')
 
-#print(valores_diferentes)
+print(valores_diferentes)
 
 
-print(df[1:])
+#print(df[1:])
 
 
 
--- reader.py
+++ reader.py
@@ -7,6 +7,8 @@ def csv_to_txt(csv_file, txt_file):
             [ my_output_file.write(" ".join(row)+'\n') for row in csv.reader(my_input_file)]
         my_output_file.close()"""
 
+
+
 def read_csv(csv_file):
     with open(csv_file, "r", encoding='utf-8') as f:
         return f.readlines()
