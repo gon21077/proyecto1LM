91d9cd64c38b6d6a0ba2fabeb93123bb65ffff91
gua21150 <gua21150@uvg.edu.gt>
2023-08-09 00:59:51 -0600

    Documentación

--- parser_1.py
+++ parser_1.py
@@ -1,25 +1,19 @@
-from reader import read_csv
-import re
-import pandas as pd
+from reader import read_csv # función realizada
+import re                   # librería de expresiones regulares
+import pandas as pd 
 import numpy as np
 
-
-data = read_csv('BL-Flickr-Images-Book.csv')
-
-
 """"
-Encontramos lso casos donde hay una coma seguida por otra coma
-
+Se encuentran los casos donde hay una coma seguida por otra coma
 """
 def coma_seguido_coma(texto):
   temp = re.sub(r'((?<=\,)(?=,))',"  ", texto) # agrega dos espacios en los casos de comas seguidas
-  temp = re.sub(r'^0{6}|^0{5}|^0{4}|^0{3}|^0{2}|^0{1}',"", temp) # elimina los 0 previamente 
+  temp = re.sub(r'^0{6}|^0{5}|^0{4}|^0{3}|^0{2}|^0{1}',"", temp) # elimina los 0 previos en el identifier
   return temp
 
 
 """
-Agarramos los ecabezados 
-
+Toma los encabezados 
 """
 def get_encabezados(d): 
     linea_encabezados = d[0]
@@ -31,17 +25,12 @@ def get_encabezados(d):
     return temp
 
 
-encabezados = get_encabezados(data)
-
 """
 Función principal que realiza la lecutura de cada linea y hace la separación de los valores que iran en cada columna
-
 """
 def parse_linea(l):
-
-    templinea = []
+    
     temp = re.findall(r'"[^"]*(?:""[^"]*)*",|["].*",|["][^"]*["]\n|[^,]*[,]|.+\n|.+', l)    #Regex de lectura CSV
-
 
     temp[-1] = temp[-1].strip("\n") #Eliminamos caracteres de nueva linea
 
@@ -52,36 +41,25 @@ def parse_linea(l):
     return temp
 
 """
-def parse_linea_mariel(l):  
-    dat = re.findall(r'(?<=[\"\[]).[^\"]*[\"\]]\,?|[^\,\"]*\,?', l) # realiza la separacion 
-    lista_filtrada = [elemento for elemento in dat if elemento != ""] # incluye los elementos que son diferentes de vacio 
-    for i in range(len(lista_filtrada)-1):
-        lista_filtrada[i] = re.sub(r'\,$|(\"\,)$',"", lista_filtrada[i]) # elimina comas y o comas comilla doble
-      
-    lista_filtrada[-1] = lista_filtrada[-1].strip("\n")
-    return lista_filtrada
-    
-"""
-
-
-
-"""
 Función que corre el programa
-
 """
 def main():
-    lineas = []
+    data = read_csv('BL-Flickr-Images-Book.csv') # lectura del set de datos
+    encabezados = get_encabezados(data) # se obtienen los encabezados 
+
+    lineas = [] # filas diferentes del encabezado
     contador = 0
     for i in range(len(data)):
         if i == 0:
             continue    
-        x = coma_seguido_coma(data[i])
-        temp = parse_linea(x)
-        lineas.append(temp)
+        x = coma_seguido_coma(data[i]) # arregla la linea por analizar
+        temp = parse_linea(x) # realiza la separacion de los campos segun la expresion regular definida
+        lineas.append(temp) 
         if len(temp)!=15:
             print("Error en linea ", i+1, ' ', len(temp))
             contador+=1
             print(temp)
+
     print("Lectura de CSV completada con ",contador, " errores")
     df = pd.DataFrame(lineas, columns=encabezados)
     df = df.replace(r'^\s*$', np.nan, regex=True)
@@ -93,6 +71,8 @@ def main():
     print(df.info())
     print("Información del dataframe completo:\n")
     print(df.shape)
+    print("Información de total datos nulos o NaN:\n")
+    print(df.isna().sum())
     print("\n")
 
 
@@ -107,40 +87,11 @@ def main():
     print(dfinicial.info())
     print("Información del dataframe completo:\n")
     print(dfinicial.shape)
-
+    print("Información de total datos nulos o NaN:\n")
+    print(dfinicial.isna().sum())
     print("\n\n")
     df.to_csv("dataframe.csv")
     print("CSV exportado!")
     return df
 
-
-
-df = main()
-
-
-#Testing: Eliminar
-
-
-dfinicial = pd.read_csv('BL-Flickr-Images-Book.csv')
-print(dfinicial.info())
-
-
-"""print(df.columns)
-print(dfinicial.columns)
-
-
-print(dfinicial.head())
-print(df.head())
-diferentes = df != dfinicial
-
-valores_diferentes = df[diferentes].fillna('Diferente')
-
-print(valores_diferentes)
-
-
-
-#print(df[1:])
-
-
-"""
-
+main()
--- reader.py
+++ reader.py
@@ -1,21 +1,4 @@
-"""import csv
-
-
-def csv_to_txt(csv_file, txt_file):
-    with open(txt_file, "w", encoding='utf-8') as my_output_file:
-        with open(csv_file, "r", encoding='utf-8') as my_input_file:
-            [ my_output_file.write(" ".join(row)+'\n') for row in csv.reader(my_input_file)]
-        my_output_file.close()"""
-
-
-
+"Realización de lectura de datos sin hacer uso de librería pandas"
 def read_csv(csv_file):
     with open(csv_file, "r", encoding='utf-8') as f:
         return f.readlines()
-
-
-
-
-
-
-
