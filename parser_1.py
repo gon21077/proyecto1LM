8f1a830c78aab516f05f3d82da63b379658a71b5
gon21077 <87709505+gon21077@users.noreply.github.com>
2023-08-08 22:19:02 -0600

    expresion completada

--- parser_1.py
+++ parser_1.py
@@ -1,16 +1,27 @@
 from reader import read_csv
 import re
 import pandas as pd
+import numpy as np
 
 
 data = read_csv('BL-Flickr-Images-Book.csv')
 
+
+""""
+Encontramos lso casos donde hay una coma seguida por otra coma
+
+"""
 def coma_seguido_coma(texto):
   temp = re.sub(r'((?<=\,)(?=,))',"  ", texto) # agrega dos espacios en los casos de comas seguidas
   temp = re.sub(r'^0{6}|^0{5}|^0{4}|^0{3}|^0{2}|^0{1}',"", temp) # elimina los 0 previamente 
   return temp
 
-def get_encabezados(d):
+
+"""
+Agarramos los ecabezados 
+
+"""
+def get_encabezados(d): 
     linea_encabezados = d[0]
     temp = []
 
@@ -19,25 +30,27 @@ def get_encabezados(d):
         temp[i] = temp[i].strip("\n")
     return temp
 
+
 encabezados = get_encabezados(data)
 
+"""
+Función principal que realiza la lecutura de cada linea y hace la separación de los valores que iran en cada columna
+
+"""
 def parse_linea(l):
 
     templinea = []
-    #temp = re.findall(r'["]+.*(.*["]["].*["]["].*)+.*["][,]|["][^"]*["][,]|["][^"]*["]\n|[^,]*[,]|.*\n', l)    
-    #temp = re.findall(r'["][^"]*["][,]|["][^"]*["]\n|[^,]*[,]|.*\n', l)
-    temp = re.findall(r'["].*["][,]|["][^"]*["]\n|[^,]*[,]|.*\n', l)
-    #temp = re.findall(r'["](.*"".*"".*)+["][,]|\n)|["][^"]*["]([,]|\n)|[^,]*([,]|\n)', l)
-    temp[-1] = temp[-1].strip("\n")
-    #print(temp)
-    #temp = [" ".join(x) for x in temp ]
-    #print(temp)
-    for i in range(len(temp)):
-        temp[i] = re.sub(r',$|["]','',temp[i])
+    temp = re.findall(r'"[^"]*(?:""[^"]*)*",|["].*",|["][^"]*["]\n|[^,]*[,]|.+\n|.+', l)    #Regex de lectura CSV
+
+
+    temp[-1] = temp[-1].strip("\n") #Eliminamos caracteres de nueva linea
+
+    for i in range(len(temp)): #Eliminamos las comas y las comillas del texto
+        temp[i] = re.sub(r',$|["]|\s','',temp[i])
     
     return temp
-#["](.*"".*"".*)+.*["][,]
 
+"""
 def parse_linea_mariel(l):  
     dat = re.findall(r'(?<=[\"\[]).[^\"]*[\"\]]\,?|[^\,\"]*\,?', l) # realiza la separacion 
     lista_filtrada = [elemento for elemento in dat if elemento != ""] # incluye los elementos que son diferentes de vacio 
@@ -46,26 +59,47 @@ def parse_linea_mariel(l):
       
     lista_filtrada[-1] = lista_filtrada[-1].strip("\n")
     return lista_filtrada
-
-
-lineas = []
-for i in range(len(data)):
-    if i == 0:
-        continue    
-    x = coma_seguido_coma(data[i])
-    temp = parse_linea(x)
     
-    lineas.append(temp)
-    if len(temp)>15:
-        print("Error en linea ", i)
-        print(temp)
-   
+"""
 
 
 
-df = pd.DataFrame(lineas, columns=encabezados)
+"""
+Función que corre el programa
 
+"""
+def main():
+    lineas = []
+    contador = 0
+    for i in range(len(data)):
+        if i == 0:
+            continue    
+        x = coma_seguido_coma(data[i])
+        temp = parse_linea(x)
+        lineas.append(temp)
+        if len(temp)!=15:
+            print("Error en linea ", i+1, ' ', len(temp))
+            contador+=1
+            print(temp)
+    print("Lectura de CSV completada con ",contador, " errores")
+    df = pd.DataFrame(lineas, columns=encabezados)
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+    df.to_csv("dataframe.csv")
+    print("CSV exportado!")
+
+
+main()
+
+
+
+""" 
+Testing: Eliminar
+"""
+"""
 dfinicial = pd.read_csv('BL-Flickr-Images-Book.csv')
+df = df.replace(r'^\s*$', np.nan, regex=True)
+
+df.to_csv("dataframe.csv")
 
 print(df.columns)
 print(dfinicial.columns)
@@ -78,6 +112,7 @@ diferentes = df != dfinicial
 valores_diferentes = df[diferentes].fillna('Diferente')
 
 print(valores_diferentes)
+"""
 
 
 #print(df[1:])
